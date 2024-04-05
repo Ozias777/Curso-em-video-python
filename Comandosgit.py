3d9ef4c20e3c4be7ceb93c8718f8a24113debb91
Ozias <oziassnascimento@gmail.com>
2024-04-05 11:17:35 -0300

    Update 1.0

---
+++ Comandosgit.py
@@ -0,0 +1,11 @@
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/Ozias777/Curso-em-video-python.git
+#git push -u origin main
+
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git push -u origin main
